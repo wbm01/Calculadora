21b5d6a1a8435c26cb896b26b1c710619c8aefbe
Willian Marioti <willian.marioti@gmail.com>
2023-03-04 15:21:03 -0300

    Cada opção escolhida chama uma função.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -5,69 +5,97 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
         double n1, n2;
         int opcao = 0;
 
-        void Menu()
+        double Soma()
         {
-            Console.Clear();
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("3 - Multiplicar");
-            Console.WriteLine("4 - Dividir");
-            Console.WriteLine("5 - Sair");
-            Console.Write("Escolha uma opção: ");
-            opcao = int.Parse(Console.ReadLine());
+            return n1 + n2;
+        }
 
-            switch (opcao)
+        double Subtracao()
+        {
+            return n1 - n2;
+        }
+
+        double Divisao()
+        {
+            return n1 / n2;
+        }
+
+        double Multiplicacao()
+        {
+            return n1 * n2;
+        }
+
+        int Menu()
             {
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
+                Console.WriteLine("5 - Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = int.Parse(Console.ReadLine());
+                
+
+                switch (opcao)
+                {
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        Console.ReadLine();
                     break;
 
-                    case 1: Console.WriteLine("A soma dos números é: "+(n1+n2));
-                    Console.ReadLine();
+                    case 1:
+                        Console.WriteLine("A soma dos números é: {0}", Soma());
+                        Console.ReadLine();
                     break;
 
-                    case 2: Console.WriteLine("A subtração dos números é: " +(n1 - n2));
-                    Console.ReadLine();
+                case 2:
+                        Console.WriteLine("A subtração dos números é: {0}", Subtracao());
+                        Console.ReadLine();
                     break;
 
-                    case 3: Console.WriteLine("A multiplicação dos números é: " +(n1*n2));
-                    Console.ReadLine();
+                    case 3:
+                        Console.WriteLine("A multiplicação dos números é: {0}", Multiplicacao());
+                        Console.ReadLine();
                     break;
 
                     case 4:
 
-                    if (n2 == 0) {
-                        Console.WriteLine("Não é possível dividir por zero!");
-                        Console.ReadLine();
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("A divisão dos números é: " + (n1 / n2));
-                        Console.ReadLine();
-                        break;
-                    }
-                    
+                        if (n2 == 0)
+                        {
+                            Console.WriteLine("Não é possível dividir por zero!");
+                            Console.ReadLine();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("A divisão dos números é: {0}", Divisao());
+                            Console.ReadLine();
+                            break;
+                        }
 
-                    case 5: Console.WriteLine("Fim do programa!");
-                    Console.ReadLine();
-                    break;
+
+                    case 5:
+                        Console.WriteLine("Fim do programa!");
+                        Console.ReadLine();
+                    break;  
+                }
+            return 0;
+            }
+
+            Console.WriteLine("Informe o primeiro número: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            while (opcao != 5)
+            {
+                Menu();
             }
         }
-
-        Console.WriteLine("Informe o primeiro número: ");
-        n1 = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Informe o segundo número: ");
-        n2 = double.Parse(Console.ReadLine());
-
-        while (opcao != 5)
-        {
-            Menu();
-        }
     }
-}
